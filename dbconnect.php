94cf4315f4f75a9808e12d2fb48cb265003f40f0
Thiago Barbosa <@thiago.dasilvabarbosa>
2026-04-27 15:19:21 +0100

    bug fixed

--- dbconnect.php
+++ dbconnect.php
@@ -9,7 +9,7 @@ $dbname = "pigment-art-gallery";
 $conn = mysqli_connect($host, $user, $pass, $dbname);
 
 if (!$conn) {
-    die("Erro na conexão com o banco de dados: " . mysqli_connect_error());
+    die("Coundn't connect with the DATABASE: " . mysqli_connect_error());
 }
 
 mysqli_set_charset($conn, "utf8mb4");
